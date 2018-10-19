fbce57e876bb03487dead9cc4ea73005a67b3cf7
sistemas3refriauto <sistemas3@refriauto.com.mx>
2018-10-19 14:08:10 -0700

    este es el codigo con la estructura base del codigo para el servicio del cfdi

---
+++ CFDI_SERVICE/Generador.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFDI_SERVICE
+{
+    class Generador
+    {
+        class generador
+        {
+            private string[] UbicacionesXml;
+            public void setXML(string path)
+            {
+                
+            }
+            public void setXML(string path, bool print)
+            {
+
+            }
+            public void setXML(string path, bool print, bool email)
+            {
+
+            }
+            public void setXML(string[] path)
+            {
+                UbicacionesXml = path;
+            }
+            public void setXML(string[] path, bool print)
+            {
+                UbicacionesXml = path;
+            }
+            public void setXML(string[] path, bool pint, bool email)
+            {
+                UbicacionesXml = path;
+            }
+
+        }
+
+
+    }
+}
