80c90ab908bfd410be264fb668929d8d073be499
sistemas3refriauto <sistemas3@refriauto.com.mx>
2018-10-19 18:23:50 -0700

    Se agrego la estructura base para las consultas de la base de datos

---
+++ CFDI_SERVICE/dataBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFDI_SERVICE
+{
+    class dataBase
+    {
+        public dataBase(){
+
+            }
+        class select
+        {
+            public void tabla(string tabla) {
+            }
+            public void dato(string tabla, string dato) {
+            }
+            public void dato(string tabla, string dato, string condicion) {
+            }
+        }
+        class query
+        {
+            public string setQuery { get;  set; }
+            public void get(string query)
+            {
+
+            }
+        }
+    }
+}
